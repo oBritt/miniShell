5e34447efd908ba5b02e0c87c1d312f7ff94ed90
oemelyan <oemelyan@student.42.fr>
2024-05-29 16:42:45 +0200

    update

--- execution/heredoc.c
+++ execution/heredoc.c
@@ -6,29 +6,29 @@
 /*   By: oemelyan <oemelyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 21:29:04 by oemelyan          #+#    #+#             */
-/*   Updated: 2024/05/27 14:30:42 by oemelyan         ###   ########.fr       */
+/*   Updated: 2024/05/29 12:58:44 by oemelyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../executor.h"
 #include "../minishell.h"
 
-void read_store_input(t_cmd *command, int nbr) //to fix that if it's only heredoc not to find paths
+void read_store_input(t_cmd *command, int nbr, t_data *data) //to fix that if it's only heredoc not to find paths
 {
 	printf("nbr = %d\n", nbr);
 
 	if (nbr == 1)
-		take_n_write(command);
+		take_n_write(command, data);
 	else if (nbr == 2)
-		do_4_two(command, nbr);
+		do_4_two(command, nbr, data);
 	else
 	{
 		skip_unused_delimiters(command, nbr);
-		do_4_two (command, nbr);
+		do_4_two (command, nbr, data);
 	}
 }
 
-void set_heredoc(t_cmd *command)
+void set_heredoc(t_cmd *command, t_data *data)
 {
 	//printf("---set heredoc---\n");
 	int		nbr;
@@ -37,7 +37,7 @@ void set_heredoc(t_cmd *command)
 	while (command->delimiter[nbr])
 		nbr++;
 	printf("nbr from set heredoc: %d\n", nbr);
-	read_store_input(command, nbr);
+	read_store_input(command, nbr, data);
 }
 
 void heredoc_check(t_data *data)
@@ -55,7 +55,7 @@ void heredoc_check(t_data *data)
 			p = pipe(data->t_cmds[i].heredoc_fd);
 			//printf("read end fd: %d, write end fd: %d\n", data->t_cmds[i].heredoc_fd[0], data->t_cmds[i].heredoc_fd[1]);
 			p_check(p, data);
-			set_heredoc(&data->t_cmds[i]);
+			set_heredoc(&data->t_cmds[i], data);
 			close(data->t_cmds[i].heredoc_fd[1]);
 		}
 		i++;
