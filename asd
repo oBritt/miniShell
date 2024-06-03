92ff2c8bcaad962c1ff7dd37c7077bf2aea706e4
Oleks <oleksandrbrittner@gmail.com>
2024-06-03 16:59:15 +0200

    a

--- asd
+++ asd
@@ -22,3 +22,38 @@ exe builtin
 aas
 exe builtin
 aas
+--get all path--
+--find in env---
+--find in env---
+Dockerfile
+Makefile
+aas
+asd
+asdsa
+builtins
+env
+execution
+executor.h
+freeing.c
+freeing.o
+in1
+in2
+init_data.c
+init_data.o
+init_env_standart.c
+init_env_standart.o
+loop.c
+loop.o
+main.c
+main.o
+minishell
+minishell.h
+out
+out1
+parsing
+signals
+start.sh
+start2.sh
+strings
+syntax
+wildcard
--- freeing.c
+++ freeing.c
@@ -6,40 +6,48 @@
 /*   By: obrittne <obrittne@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 11:11:30 by obrittne          #+#    #+#             */
-/*   Updated: 2024/06/03 16:45:14 by obrittne         ###   ########.fr       */
+/*   Updated: 2024/06/03 16:56:16 by obrittne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	free_s(t_cmd *cmd, int e)
+{
+	if (cmd[e].delimiter)
+		freeing(cmd[e].delimiter);
+	if (cmd[e].input_redirect)
+		freeing(cmd[e].input_redirect);
+	if (cmd[e].output_redirect)
+		freeing(cmd[e].output_redirect);
+	if (cmd[e].is_ambigious_input)
+		free(cmd[e].is_ambigious_input);
+	if (cmd[e].is_ambigious_output)
+		free(cmd[e].is_ambigious_output);
+	if (cmd[e].cmd)
+		freeing(cmd[e].cmd);
+	if (cmd[e].is_output_append)
+		free(cmd[e].is_output_append);
+	if (cmd[e].output_redirect[0])
+		close(cmd[0].out_fd);
+	if (cmd[e].input_redirect[0])
+		close(cmd[0].in_fd);
+}
+
 void	freeing_cmds(t_cmd *cmd)
 {
 	int	e;
 
-	e = 0;
-	while (cmd[0].amount > e)
+	if (cmd)
 	{
-		if (cmd[e].delimiter)
-			freeing(cmd[e].delimiter);
-		if (cmd[e].input_redirect)
-			freeing(cmd[e].input_redirect);
-		if (cmd[e].output_redirect)
-			freeing(cmd[e].output_redirect);
-		if (cmd[e].is_ambigious_input)
-			free(cmd[e].is_ambigious_input);
-		if (cmd[e].is_ambigious_output)
-			free(cmd[e].is_ambigious_output);
-		if (cmd[e].cmd)
-			freeing(cmd[e].cmd);
-		if (cmd[e].is_output_append)
-			free(cmd[e].is_output_append);
-		if (cmd[e].output_redirect[0])
-			close(cmd[0].out_fd);
-		if (cmd[e].input_redirect[0])
-			close(cmd[0].in_fd);
-		e++;
+		e = 0;
+		while (cmd[0].amount > e)
+		{
+			free_s(cmd, e);
+			e++;
+		}
+		free(cmd);
 	}
-	free(cmd);
 }
 
 void	free_data(t_data *data)
